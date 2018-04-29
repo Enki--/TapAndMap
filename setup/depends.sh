8dee021f83650c0edc337aaa6112624302e75f88
enki-- <pbvinge@gmail.com>
2018-04-28 14:12:38 -1000

    creating bootstrap files

---
+++ setup/depends.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+current_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [[ "$EUID" != "0" ]]; then
+  echo "[!] This script must be run as root." 1>&2
+  exit 1
+fi
+
+if [[ $OSTYPE == "linux-gnu" ]]; then
+  echo -e "\t#########################"
+  echo -e "\t   linux Configuration"
+  echo -e "\t#########################"
+
+  echo "[*] Installing python..."
+  apt-get install python3 python-pip -y
+
+  echo "[*] Installing packages..."
+  apt-get install geoip-bin geoip-database apache2 tcpdump screen python-pcapy bridge-utils -y
+
+  echo "[*] Installing python libs..."
+  sh "$current_path/python3-depends.sh"
+
+  echo ""
+  echo "[*] Setup Completed."
---
+++ setup/python3-depends.sh
@@ -0,0 +1,3 @@
+#!/bin/#!/usr/bin/env bash
+
+# template file, I'm sure i will have some
