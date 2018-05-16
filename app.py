36fcd7a9ba588ef2674119981c3055f2ca421299
enki-- <pbvinge@gmail.com>
2018-05-16 12:35:44 -0500

    lines finally working

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 # coding: utf-8
+#!/usr/bin/env python3
 
-from flask import Flask, render_template, Response
+import redis
+import configparser
 from flask_googlemaps import GoogleMaps
 from flask_googlemaps import Map, icons
-import configparser
-import redis
+from flask import Flask, render_template, Response
+
 
 app = Flask(__name__, template_folder="templates")
 
 # you can set key as config
-app.config['GOOGLEMAPS_KEY'] =
-GoogleMaps(app, key="")
+app.config['GOOGLEMAPS_KEY'] = "Add Key"
+GoogleMaps(app, key="Add Key")
 
 config = configparser.ConfigParser()
 config.read('TapAndMap.conf')
@@ -18,43 +20,23 @@ config.read('TapAndMap.conf')
 
 @app.route("/")
 def mapview():
-    geodesic_line = {
-        'stroke_color': '#00FF00',
-        'stroke_opacity': 1.0,
-        'stroke_weight': 3,
-        'geodesic': True,
-        'path': [(float(config['all']['HomeLat']),
-                  float(config['all']['HomeLong'])),
-                 (37.4300, -122.1400)
-                 ]
-    }
-
-    geodesic_line2 = {
-        'stroke_color': '#0000FF',
-        'stroke_opacity': 1.0,
-        'stroke_weight': 3,
-        'geodesic': True,
-        'path': [(float(config['all']['HomeLat']),
-                  float(config['all']['HomeLong'])),
-                 (60.4300, -80.1400)
-                 ]
-    }
-
     r = redis.StrictRedis(host='localhost', port=6379, db=0)
     markersL = [{'icon': icons.alpha.A,
                  'lat': config['all']['HomeLat'],
                  'lng': config['all']['HomeLong'],
                  'infobox': "This is your TapAndMap server.  IP: " +
                  config['all']['TapAndMapIP']}]
-    polyL = [geodesic_line, geodesic_line2]
-
+    geolines = []
     for key in r.scan_iter("*"):
         if key.split(b':')[1] == b'1':  # ICMP
             icon = icons.dots.blue
+            line = '#0000FF'
         elif key.split(b':')[1] == b'6':  # TCP
             icon = icons.dots.yellow
+            line = '#FFFF00'
         elif key.split(b':')[1] == b'17':  # UDP
             icon = icons.dots.green
+            line = '#00FF00'
         try:
             markersL.append({'icon': icon,
                              'lat': r.get(key).split(b'x')[0].decode("UTF-8"),
@@ -62,17 +44,21 @@ def mapview():
                              'infobox': 'IP:' +
                              key.split(b':')[0].decode("UTF-8"),
                              })
-            polyL2 = {'stroke_color': '#0000FF',
-                      'stroke_opacity': 1.0,
-                      'stroke_weight': 3,
-                      'geodesic': True,
-                      'path': [(float(config['all']['HomeLat']),
-                                float(config['all']['HomeLong'])),
-                               (r.get(key).split(b'x')[0].decode("UTF-8"),
-                                r.get(key).split(b'x')[1].decode("UTF-8"))
-                               ]
-                      }
 
+            pathList = [{'lat': float(config['all']['HomeLat']),
+                         'lng': float(config['all']['HomeLong'])},
+                        {'lat': float(
+                                r.get(key).split(b'x')[0].decode("UTF-8")),
+                         'lng': float(
+                                r.get(key).split(b'x')[1].decode("UTF-8"))}]
+
+            geolines.append({'stroke_color': line,
+                             'stroke_opacity': 1.0,
+                             'stroke_weight': 3,
+                             'geodesic': True,
+                             'path': pathList
+                             }
+                            )
         except IndexError:
             pass
 
@@ -84,7 +70,7 @@ def mapview():
         style="height:100vh;width:70vw;margin:0;float:left;",
         zoom=config['all']['ZoomLevel'],
         fit_markers_to_bounds=True,
-        polylines=polyL,
+        polylines=geolines,
         markers=markersL
     )
 
